195088069fe1b6edc80b8b60a473e4c39262fb54
aoxipo <407157175@qq.com>
2023-10-25 18:36:00 +0800

    add file strucy

---
+++ dataloader.py
@@ -0,0 +1,12 @@
+
+from torch.utils.data import Dataset
+
+class DataLoad(Dataset):
+    def __init__(self) -> None:
+        pass
+
+    def __len__(self):
+        pass
+
+    def __getitem__(self, index):
+        pass
